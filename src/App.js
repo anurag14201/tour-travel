73433f1c9582a0098d1f97cc014a1a635d52b1f4
anurag14201 <108300944+anurag14201@users.noreply.github.com>
2022-10-25 14:58:43 +0600

    Title

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './Component/Header/Header';
 
@@ -5,6 +6,12 @@ function App() {
   return (
     <div className=''>
       <Header></Header>
+      <Routes>
+        <Route path=''></Route>
+        <Route></Route>
+        <Route></Route>
+        <Route></Route>
+      </Routes>
     </div>
   );
 }
